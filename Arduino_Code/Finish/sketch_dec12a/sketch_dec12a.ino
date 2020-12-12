017b405bda1b5d2caf6cf9be3e578503c1e7c890
jseoup <crobat126@naver.com>
2020-12-12 20:21:17 +0900

    Update sketch_dec12a.ino

--- Arduino_Code/Finish/sketch_dec12a/sketch_dec12a.ino
+++ Arduino_Code/Finish/sketch_dec12a/sketch_dec12a.ino
@@ -12,27 +12,32 @@ void setup()
   pinMode(A_2A, OUTPUT);
 }
 
-void loop() 
+void loop()
 {
-  sensorVal = analogRead(A0Pin);  // 토양센서값 읽어 저장
+  sensorVal = analogRead(A0Pin);  // 토양 센서 값 읽어 저장
   delay(1000);
-  Serial.print("Asensor = ");
-  Serial.println(sensorVal);  // 0(습함) ~ 1023(건조)값 출력 
+  //Serial.print("Asensor = ");
+  //Serial.println(sensorVal);  // 0(습함) ~ 1023(건조)값 출력 
+
+  // 토양이 습할 경우
   if ( sensorVal <= 500) 
   {    
-    Serial.println(" Very Wet ! ");        
+    //Serial.println(" Very Wet ! ");  
+    Serial.println(sensorVal); // 토양의 습도 값(수치) 콘솔에 출력    
     stop();   
   }
+  // 토양이 건조할 경우
   else if ( sensorVal > 500)
   {
-    Serial.println(" Very Dry ! ");    
-    pump(1);
-    Serial.println(" Pump On for 1 Second!");    
-    delay(1000);
+    //Serial.println(" Very Dry ! ");  
+    Serial.println(sensorVal); // 토양의 습도 값(수치) 콘솔에 출력   
+    pump(1); // 워터 펌프 작동
+    //Serial.println(" Pump On for 5 Second!");    
+    delay(5000); // 5초 주기로 딜레이
   }    
 }
   
-
+// 워터 펌프 작동 함수
 void pump(int flag)
 {
   boolean inPin1 = HIGH;
