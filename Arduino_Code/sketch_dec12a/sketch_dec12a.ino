c9d74b0763f6b86733b0a918162dff72aee82a7c
tnflth <71124678+tnflth@users.noreply.github.com>
2020-12-12 19:51:36 +0900

    update code

--- Arduino_Code/sketch_dec12a/sketch_dec12a.ino
+++ Arduino_Code/sketch_dec12a/sketch_dec12a.ino
@@ -1,18 +1,35 @@
+/* Auto Water Pot - 자동 물공급 화분 */
+#define A0Pin 0
+int sensorVal = 0;
 int A_1A = 11;
 int A_2A = 12;
 
 void setup()
 {
+  Serial.begin(9600);
   pinMode(4, INPUT);
   pinMode(A_1A, OUTPUT);
   pinMode(A_2A, OUTPUT);
 }
 
-void loop() {
-  pump(1);
+void loop() 
+{
+  sensorVal = analogRead(A0Pin);  // 토양센서값 읽어 저장
   delay(1000);
-  stop();
-  delay(1000);
+  Serial.print("Asensor = ");
+  Serial.println(sensorVal);  // 0(습함) ~ 1023(건조)값 출력 
+  if ( sensorVal <= 500) 
+  {    
+    Serial.println(" Very Wet ! ");        
+    stop();   
+  }
+  else if ( sensorVal > 500)
+  {
+    Serial.println(" Very Dry ! ");    
+    pump(1);
+    Serial.println(" Pump On for 1 Second!");    
+    delay(1000);
+  }    
 }
   
 
